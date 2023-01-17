763646d145a7a2f79db2bac1b66166d8191add7e
Kroumch <122924179+Kroumch@users.noreply.github.com>
2023-01-17 20:59:19 +0200

    Add files via upload

---
+++ baigiamasis-darbas/prices.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kainos</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav>
+    <h1>
+        <a href="index.php">beSTRONG</a>
+    </h1>
+    <ul>
+        <li class="active"><a href="index.php">Pradžia</a></li>
+        <li><a href="trainings.php">Treniruotės</a></li>
+        <li><a href="prices.php">Kainos</a></li>
+        <li><a href="timetable.php">Tvarkaraštis</a></li>
+        <li><a href="news.php">Naujienos</a></li>
+        <li><a href="duk.php">D.U.K.</a></li>
+        <li><a href="login.php">Prisijungti</a></li>
+    </ul>
+</nav>
+
+<div class="general-section">
+    <div class="container">
+        <div class="title-part">
+            <h1>Kainos</h1>
+            <div class="orange-row"></div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus, explicabo.</p>
+        </div>
+        <div class="content-part">
+        <div class="price-block title-part">
+            <p>Apsilankymas pirtyje</p>
+            <div class="black-row"></div>
+            <h1>14 €</h1>
+        </div>
+        <div class="price-block title-part">
+            <p>Apsilankymas baseine</p>
+            <div class="black-row"></div>
+            <h1>17 €</h1>
+        </div>
+        <div class="price-block title-part">
+            <p>Viena treniruotė<br><br></p>
+            <div class="black-row"></div>
+            <h1>12 €</h1>
+        </div>
+        <div class="price-block title-part">
+            <p>1 mėnuo<br><br></p>
+            <div class="black-row"></div>
+            <h1>29.99 €</h1>
+        </div>
+        <div class="price-block title-part">
+            <p>6 mėnesiai<br><br></p>
+            <div class="black-row"></div>
+            <h1>120 €</h1>
+        </div>
+        <div class="price-block title-part">
+            <p>12 mėnesių<br><br></p>
+            <div class="black-row"></div>
+            <h1>180 €</h1>
+        </div>
+        </div>
+        
+    </div>
+</div>
+
+<footer class="general-section">
+    <ul>
+        <li class="active"><a href="index.php">Pradžia</a></li>
+        <li><a href="trainings.php">Treniruotės</a></li>
+        <li><a href="prices.php">Kainos</a></li>
+        <li><a href="timetable.php">Tvarkaraštis</a></li>
+        <li><a href="news.php">Naujienos</a></li>
+        <li><a href="duk.php">D.U.K.</a></li>
+        <li><a href="#">Prisijungti</a></li>
+    </ul>
+
+    <div class="partneriai title-part">
+        <h1>Partneriai</h1>
+        <div class="orange-row"></div>
+        <p>Nicke</p>
+        <p>Ardidas</p>
+        <p>Olimpik Geyms</p>
+    </div>
+</footer>
+
+
+    <script src="js/scripts.js"></script>
+</body>
+</html>
---
+++ baigiamasis-darbas/timetable.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tvarkaraštis</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav>
+    <h1>
+        <a href="index.php">beSTRONG</a>
+    </h1>
+    <ul>
+        <li class="active"><a href="index.php">Pradžia</a></li>
+        <li><a href="trainings.php">Treniruotės</a></li>
+        <li><a href="prices.php">Kainos</a></li>
+        <li><a href="timetable.php">Tvarkaraštis</a></li>
+        <li><a href="news.php">Naujienos</a></li>
+        <li><a href="duk.php">D.U.K.</a></li>
+        <li><a href="login.php">Prisijungti</a></li>
+    </ul>
+</nav>
+
+<div class="general-section">
+    <div class="container">
+        <div class="title-part">
+            <h1>Tvarkaraštis</h1>
+            <div class="orange-row"></div>
+        </div>
+        <div class="table-block">
+        <table class="content-table">
+            <tr>
+                <thead>
+                    <th></th>
+                    <th>Pirmadienis</th>
+                    <th>Antradienis</th>
+                    <th>Trečiadienis</th>
+                    <th>Ketvirtadienis</th>
+                    <th>Penktadienis</th>
+                    <th>Šeštadienis</th>
+                    <th>Sekmadienis</th>
+                </thead>
+            </tr>
+            <tr>
+                <th>08:00</th>
+                <td>Crossfitas</td>
+                <td>Aerobika</td>
+                <td>Crossfitas</td>
+                <td>Aerobika</td>
+                <td>Crossfitas</td>
+                <td>Aerobika</td>
+                <td>Crossfitas</td>
+            </tr>
+            <tr class="active-row">
+                <th>10:00</th>
+                <td>Cardio</td>
+                <td>Boksas</td>
+                <td>Cardio</td>
+                <td>Boksas</td>
+                <td>Cardio</td>
+                <td>Boksas</td>
+                <td>Cardio</td>
+            </tr>
+            <tr>
+                <th>12:00</th>
+                <td>Joga</td>
+                <td>Pilatesas</td>
+                <td>Joga</td>
+                <td>Pilatesas</td>
+                <td>Joga</td>
+                <td>Pilatesas</td>
+                <td>Joga</td>
+            </tr>
+            <tr class="active-row">
+                <th>14:00</th>
+                <td>Aerobika</td>
+                <td>Crossfitas</td>
+                <td>Aerobika</td>
+                <td>Crossfitas</td>
+                <td>Aerobika</td>
+                <td>Crossfitas</td>
+                <td>Aerobika</td>
+            </tr>
+            <tr>
+                <th>16:00</th>
+                <td>Boksas</td>
+                <td>Cardio</td>
+                <td>Boksas</td>
+                <td>Cardio</td>
+                <td>Boksas</td>
+                <td>Cardio</td>
+                <td>Boksas</td>
+            </tr>
+            <tr class="active-row">
+                <th>18:00</th>
+                <td>Pilatesas</td>
+                <td>Joga</td>
+                <td>Pilatesas</td>
+                <td>Joga</td>
+                <td>Pilatesas</td>
+                <td>Joga</td>
+                <td>Pilatesas</td>
+            </tr>
+
+</table>
+        </div>
+    </div>
+</div>
+
+
+<footer class="general-section">
+    <ul>
+        <li class="active"><a href="index.php">Pradžia</a></li>
+        <li><a href="trainings.php">Treniruotės</a></li>
+        <li><a href="prices.php">Kainos</a></li>
+        <li><a href="timetable.php">Tvarkaraštis</a></li>
+        <li><a href="news.php">Naujienos</a></li>
+        <li><a href="duk.php">D.U.K.</a></li>
+        <li><a href="#">Prisijungti</a></li>
+    </ul>
+
+    <div class="partneriai title-part">
+        <h1>Partneriai</h1>
+        <div class="orange-row"></div>
+        <p>Nicke</p>
+        <p>Ardidas</p>
+        <p>Olimpik Geyms</p>
+    </div>
+</footer>
+
+
+    <script src="js/scripts.js"></script>
+</body>
+</html>
